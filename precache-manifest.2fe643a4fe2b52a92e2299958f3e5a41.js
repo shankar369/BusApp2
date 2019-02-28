f8d28a8f6665e7cf98f116b971222deda8c51a6b
Durga Sankar Pasupuleti <durga.pasupuleti@corp.ads.valuelabs.net>
2019-02-28 10:35:07 +0530

    Updates

---
+++ precache-manifest.2fe643a4fe2b52a92e2299958f3e5a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "d12fc4f1a488e2fe7e44",
+    "url": "./static/js/main.d12fc4f1.chunk.js"
+  },
+  {
+    "revision": "e3309c8837db056de4c7",
+    "url": "./static/js/2.e3309c88.chunk.js"
+  },
+  {
+    "revision": "d12fc4f1a488e2fe7e44",
+    "url": "./static/css/main.6f42444b.chunk.css"
+  },
+  {
+    "revision": "107c8e6f0b034e5a06536cd501988878",
+    "url": "./index.html"
+  }
+];
